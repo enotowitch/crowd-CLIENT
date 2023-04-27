7697d552f042c32a78267e7c2ed6229c93cb50dd
enotowitch <en.enotowitch@gmail.com>
2023-04-27 12:47:34 +0300

    test share 6

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -1,16 +1,26 @@
 import React from "react"
 import "./index.scss"
 import "./media.scss"
-import { FacebookShareButton, FacebookIcon } from "react-share";
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } from "react-share";
 
 export default function Share({ title, tag }) {
 	return (
-		<FacebookShareButton
-			url={window.location.href}
-			quote={"quote"}
-			hashtag={tag}
-		>
-			<FacebookIcon size="26" round="true"></FacebookIcon>
-		</FacebookShareButton>
+		<>
+			<FacebookShareButton
+				url={window.location.href}
+				quote={"quote"}
+				hashtag={"tag"}
+			>
+				<FacebookIcon size="26" round="true" />
+			</FacebookShareButton>
+
+			<TwitterShareButton
+				url={window.location.href}
+				title={"title"}
+				hashtags={["tag", tag]}
+			>
+				<TwitterIcon size="26" round="true" />
+			</TwitterShareButton>
+		</>
 	)
 }

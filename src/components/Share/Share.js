4ccb81393fe549a01339c4eb287fb583f509d121
enotowitch <en.enotowitch@gmail.com>
2023-04-27 12:24:45 +0300

    test share 4

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -1,17 +1,15 @@
 import React from "react"
 import "./index.scss"
 import "./media.scss"
-import { LinkedinShareButton, LinkedinIcon } from "react-share";
+import { FacebookShareButton, FacebookIcon } from "react-share";
 
 export default function Share({ title }) {
 	return (
-		<LinkedinShareButton
+		<FacebookShareButton
 			url={window.location.href}
-			title={title}
-			summary="Description of the shared page"
-			source={window.location.host}
+			quote={title}
 		>
-			<LinkedinIcon size="26" round="true"></LinkedinIcon>
-		</LinkedinShareButton>
+			<FacebookIcon size="26" round="true"></FacebookIcon>
+		</FacebookShareButton>
 	)
 }

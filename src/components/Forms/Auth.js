bbabe1f3fdff595ac5d513e76d844f13d436859c
enotowitch <en.enotowitch@gmail.com>
2023-04-28 11:09:59 +0300

    M

--- src/components/Forms/Auth.js
+++ src/components/Forms/Auth.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./index.scss"
 import "./media.scss"
 import Input from "../FormElements/Input"
@@ -6,15 +6,17 @@ import IconText from "../IconText/IconText"
 import useAuth from "../../hooks/useAuth"
 import ErrorMsg from "../FormElements/ErrorMsg"
 import AuthNew from "./AuthNew"
+import { Context } from "../../Context"
 
 
 export default function Auth({ children, type }) { // type=Log In/Sign Up
 
 	const { onSubmit, errorMsg } = useAuth(type)
+	const { popupElementSet } = useContext(Context)
 
 	return (
 		<form className="auth" onSubmit={onSubmit}>
-			<IconText src="logo2" text="Copilot Funding" className="fc g black" />
+			<IconText src="logo2" text="Copilot Funding" className="fc g black" iconClassName="m0" onClick={() => popupElementSet(false)} />
 
 			<Input name="email" label="Email" type="email" placeholder="Enter Email" required />
 			<Input name="password" label="Password" type="password" placeholder="Enter Password" required />

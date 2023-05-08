288e29e40f04d5f2544ccc7223da43e76d1f8d16
enotowitch <en.enotowitch@gmail.com>
2023-05-08 16:33:47 +0300

    +showMore FIX

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import "./index.scss"
 import "./media.scss"
 import AddComment from "./AddComment"
@@ -6,12 +6,10 @@ import useComments from "../../hooks/useComments"
 import Comment from "./Comment"
 import ScrollTo from "../Navs/ScrollTo"
 import ShowMore from "../FormElements/ShowMore"
-import { skip_ } from "../../consts"
 
 export default function Comments() {
 
-	const [skip, skipSet] = useState(0)
-	const { comments } = useComments(skip)
+	const { comments, showMore } = useComments()
 
 	return (
 		<section className="comments">
@@ -20,7 +18,7 @@ export default function Comments() {
 
 			{comments?.map(obj => <Comment key={obj._id} obj={obj} />)}
 
-			<ShowMore onClick={() => skipSet(prev => prev + skip_)} />
+			<ShowMore onClick={showMore} />
 
 			<AddComment />
 
--- src/hooks/useComments.js
+++ src/hooks/useComments.js
@@ -4,10 +4,16 @@ import { Context } from "../Context"
 import hideShowMore from "../utils/hideShowMore"
 
 // * watchingPost is set in hook use...Full.js
-export default function useComments(skip) {
+export default function useComments() {
 
 	const [comments, commentsSet] = useState([])
 	const { watchingPost } = useContext(Context)
+
+	// onClick to btn showMore skip+=5 => goes to server => skip 5,10,15...
+	const [skip, skipSet] = useState(0)
+	function showMore() {
+		skipSet(prev => prev + 5) // * 0+5/5+5/10+5/15+5...
+	}
 
 	useEffect(() => {
 		async function getComments() {
@@ -21,6 +27,6 @@ export default function useComments(skip) {
 
 
 	return (
-		{ comments }
+		{ comments, showMore }
 	)
 }

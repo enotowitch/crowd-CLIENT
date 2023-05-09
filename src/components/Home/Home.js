c581c0a2f2bb27c920e7194f8dd3dd835bda92b4
enotowitch <en.enotowitch@gmail.com>
2023-05-09 18:46:46 +0300

    M

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,14 @@ import "./index.scss"
 import "./media.scss"
 import Companies from "../Company/Companies"
 import Articles from "../Articles/Articles"
+import Bonuses from "../Bonus/Bonuses"
 
 export default function Home() {
 	return (
 		<div className="fc g">
 			<Companies />
 			<Articles />
+			<Bonuses />
 		</div>
 	)
 }

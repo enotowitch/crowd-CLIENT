319aab98bf360c647a5e66945b14ffd1d775de0d
enotowitch <en.enotowitch@gmail.com>
2023-05-09 11:37:34 +0300

    +Share(changed)

--- src/components/Forms/ShareArticle.js
+++ src/components/Forms/ShareArticle.js
@@ -1,56 +1,19 @@
 import React, { useContext } from "react"
 import "./index.scss"
 import "./media.scss"
-import { FacebookShareButton, TwitterShareButton, LinkedinShareButton } from "react-share"
-import IconText from "../Icons/IconText"
 import { Context } from "../../Context"
 import parseImg from "../../utils/parseImg"
-import InputButton from "../FormElements/InputButton"
+import Share from "./Share"
 
 export default function ShareArticle() {
 
-	const url = window.location.href
 	const { sharing } = useContext(Context)
 	const { title, tag, value } = sharing
 	const img = parseImg(value)
 
 	return (
-		<div className="share">
-
-			<div className="title">{tag}</div>
+		<Share topTitle={tag} bottomTitle={title} hashtag={tag}>
 			<img src={img} />
-			<div className="title">Share: {title}</div>
-
-			<div className="f jcsb">
-				<TwitterShareButton
-					url={url}
-					title={title}
-					hashtags={[tag]}
-					style={{ background: "#00aced" }}
-				>
-					<IconText src="twitter" iconClassName="m0 c" />
-				</TwitterShareButton>
-
-				<FacebookShareButton
-					url={url}
-					quote={title}
-					hashtag={tag}
-					style={{ background: "#3b5998" }}
-				>
-					<IconText src="facebook" iconClassName="m0 c" />
-				</FacebookShareButton>
-
-				<LinkedinShareButton
-					url={url}
-					title={title}
-					hashtag={tag}
-					style={{ background: "#0072b1" }}
-				>
-					<IconText src="linkedin" iconClassName="m0 c" />
-				</LinkedinShareButton>
-			</div>
-
-			<InputButton text="Copy" value={url} onClick={() => navigator.clipboard.writeText(url)} />
-		</div>
+		</Share>
 	)
 }
--- src/components/Forms/ShareCompany.js
+++ src/components/Forms/ShareCompany.js
@@ -1,55 +1,18 @@
 import React, { useContext } from "react"
 import "./index.scss"
 import "./media.scss"
-import { FacebookShareButton, TwitterShareButton, LinkedinShareButton } from "react-share"
-import IconText from "../Icons/IconText"
 import { Context } from "../../Context"
-import InputButton from "../FormElements/InputButton"
 import Radar from "../Charts/Radar"
+import Share from "./Share"
 
 export default function ShareCompany() {
 
-	const url = window.location.href
 	const { sharing } = useContext(Context)
 	const { name } = sharing
 
 	return (
-		<div className="share">
-
-			<div className="title">{name}</div>
+		<Share topTitle={name} bottomTitle={name} hashtag={name}>
 			<Radar obj={sharing} />
-			<div className="title">Share: {name} review</div>
-
-			<div className="f jcsb">
-				<TwitterShareButton
-					url={url}
-					title={name}
-					hashtags={[name]}
-					style={{ background: "#00aced" }}
-				>
-					<IconText src="twitter" iconClassName="m0 c" />
-				</TwitterShareButton>
-
-				<FacebookShareButton
-					url={url}
-					quote={name}
-					hashtag={name}
-					style={{ background: "#3b5998" }}
-				>
-					<IconText src="facebook" iconClassName="m0 c" />
-				</FacebookShareButton>
-
-				<LinkedinShareButton
-					url={url}
-					title={name}
-					hashtag={name}
-					style={{ background: "#0072b1" }}
-				>
-					<IconText src="linkedin" iconClassName="m0 c" />
-				</LinkedinShareButton>
-			</div>
-
-			<InputButton text="Copy" value={url} onClick={() => navigator.clipboard.writeText(url)} />
-		</div>
+		</Share>
 	)
 }

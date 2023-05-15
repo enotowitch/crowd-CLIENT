a9216bfdd8b03f06d724c6076e297baf23416a16
enotowitch <en.enotowitch@gmail.com>
2023-05-15 15:11:27 +0300

    Menu/ProfileMenu FIX

--- src/components/Navs/Menu.js
+++ src/components/Navs/Menu.js
@@ -1,22 +1,32 @@
-import React, { useContext } from "react"
+import React, { useContext, useRef } from "react"
 import "./index.scss"
 import "./media.scss"
 import NavBottom from "./NavBottom"
+import NavTop from "./NavTop"
 import { Context } from "../../Context"
 import HeaderButtons from "./HeaderButtons"
 import ProfileTop from "../Profile/ProfileTop"
 
 export default function Menu() {
 
-	const { showMenu } = useContext(Context)
+	const { showMenu, showProfileMenuSet } = useContext(Context)
+
+	// ! hide ProfileMenu on scroll (mobile)
+	const ref = useRef(null)
+	ref?.current?.addEventListener("scroll", () => {
+		showProfileMenuSet(false)
+	})
 
 	return (
 		showMenu &&
 
 		<div className="menu">
-			<ProfileTop />
-			<HeaderButtons />
-			<NavBottom />
+			<div className="menu__scroll" ref={ref}>
+				<ProfileTop />
+				<HeaderButtons />
+				<NavTop />
+				<NavBottom />
+			</div>
 		</div>
 	)
 }

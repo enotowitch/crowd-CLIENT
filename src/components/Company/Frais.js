ce33bf7b4c2a08ac207ba46364e2a05563172ebf
enotowitch <en.enotowitch@gmail.com>
2023-05-16 14:31:24 +0300

    F: edit Frais Legends

--- src/components/Company/Frais.js
+++ src/components/Company/Frais.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./index.scss"
 import "./media.scss"
 import Input from "../FormElements/Input"
@@ -8,12 +8,33 @@ export default function Frais({ obj }) { // obj=company
 
 	const [count, countSet] = useState(1)
 
+	// ! edit Frais Legends
+	let editFraisInputsLength
+	if (obj) {
+		// * obj?.FraisObj = {name 1: '1', name 2: '2'}
+		editFraisInputsLength = Object.keys(obj?.FraisObj).length // * ["name1", "name2"].length
+	}
+
+	useEffect(() => {
+		countSet(editFraisInputsLength)
+	}, [obj])
+	// ? edit Frais Legends
+
 	function inputs() {
 		return Array.from({ length: count }, (v, i) => i).map((item, ind) => {
+
+			// ! edit Frais Legends
+			let editFraisInputName, editFraisInputValue
+			if (obj) {
+				editFraisInputName = Object.keys(obj?.FraisObj)
+				editFraisInputValue = Object.values(obj?.FraisObj)
+			}
+			// ? edit Frais Legends
+
 			return (
 				<>
-					<Input name={`FraisLegend${ind + 1}name`} placeholder={`name of legend ${ind + 1}`} />
-					<Input name={`FraisLegend${ind + 1}value`} placeholder={`value of legend ${ind + 1}`} />
+					<Input name={`FraisLegend${ind + 1}name`} placeholder={`name of legend ${ind + 1}`} editValue={editFraisInputName?.[ind]} />
+					<Input name={`FraisLegend${ind + 1}value`} placeholder={`value of legend ${ind + 1}`} editValue={editFraisInputValue?.[ind]} />
 				</>
 			)
 		})

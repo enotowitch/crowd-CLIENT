9a53200b16c379c5062e1ca73042a732b2c92800
enotowitch <en.enotowitch@gmail.com>
2023-05-17 10:52:10 +0300

    +Remember +Checkbox(+)

--- src/components/FormElements/Checkbox.js
+++ src/components/FormElements/Checkbox.js
@@ -1,19 +1,32 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./index.scss"
 import "./media.scss"
 
-export default function Checkbox({ name }) {
+export default function Checkbox(props) {
 
-	const [value, valueSet] = useState(false)
+	const { name, rememberChecked } = props
+
+	const [checked, checkedSet] = useState(false)
+
+	function onChangeFew(e) {
+		checkedSet(prev => !prev)
+		props.onChange && props.onChange(e)
+	}
+	// * render checked/notChecked if `rememberChecked` prop passed
+	useEffect(() => {
+		checkedSet(rememberChecked)
+	}, [rememberChecked])
 
 	return (
 		<label className="if wsnw mr wfc">
 			<input
 				type="checkbox"
 				name={name}
-				value={value}
-				onChange={() => valueSet(prev => !prev)}
+				checked={checked}
 				className="mr05"
+				{...props}
+				// ! onChange MUST be last
+				onChange={onChangeFew}
 			/>
 			<span>{name}</span>
 		</label>
--- src/components/Forms/Login.js
+++ src/components/Forms/Login.js
@@ -2,13 +2,14 @@ import React from "react"
 import "./index.scss"
 import "./media.scss"
 import Auth from "./Auth"
+import Remember from "../FormElements/Remember"
 
 export default function Login() {
 	return (
 		<>
 			<Auth type="Log In">
 				<div className="f jcsb">
-					<div>Remember Me</div>
+					<Remember />
 					<div className="brand fw700">Forgot Password</div>
 				</div>
 			</Auth>

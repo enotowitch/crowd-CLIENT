18ce9538647872420964feafe38f294287a5f106
enotowitch <en.enotowitch@gmail.com>
2023-05-17 14:51:48 +0300

    TEST: appRouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react"
-import Articles from "./components/Articles/Articles"
-import ArticleFull from "./components/Articles/ArticleFull"
-import { Routes, Route } from "react-router-dom"
 import NavBottom from "./components/Navs/NavBottom"
 import Header from "./components/Navs/Header"
 import Banner from "./components/Banner/Banner"
 import Popup from "./components/Popup/Popup"
 import Menu from "./components/Navs/Menu"
-import AddArticle from "./components/Articles/AddArticle"
 import Footer from "./components/Navs/Footer"
-import AddCompany from "./components/Company/AddCompany"
-import Companies from "./components/Company/Companies"
-import CompanyFull from "./components/Company/CompanyFull"
 import ScrollTo from "./components/Navs/ScrollTo"
-import Home from "./components/Home/Home"
-import AddBonus from "./components/Bonus/AddBonus"
-import Bonuses from "./components/Bonus/Bonuses"
-import BonusFull from "./components/Bonus/BonusFull"
-import Aboutus from "./components/Aboutus/Aboutus"
 import NavTop from "./components/Navs/NavTop"
-import AddInvestment from "./components/Investment/AddInvestment"
-import Writeus from "./components/Writeus/Writeus"
-import Dashboard from "./components/Dashboard/Dashboard"
+import AppRouter from "./components/AppRouter/AppRouter"
 
 export default function App() {
 
@@ -40,26 +26,7 @@ export default function App() {
 					<NavBottom />
 				</div>
 
-				<Routes>
-					<Route exact path="/" element={<Home />} />
-					<Route exact path="add-article" element={<AddArticle />} />
-					<Route exact path="articles" element={<Articles />} />
-					<Route exact path="article/:articleId" element={<ArticleFull />} />
-					<Route exact path="editArticle/:articleId" element={<AddArticle />} />
-					<Route exact path="add-company" element={<AddCompany />} />
-					<Route exact path="companies" element={<Companies />} />
-					<Route exact path="company/:companyId" element={<CompanyFull />} />
-					<Route exact path="editCompany/:companyId" element={<AddCompany />} />
-					<Route exact path="add-bonus" element={<AddBonus />} />
-					<Route exact path="bonuses" element={<Bonuses />} />
-					<Route exact path="bonus/:bonusId" element={<BonusFull />} />
-					<Route exact path="editBonus/:bonusId" element={<AddBonus />} />
-					<Route exact path="aboutus" element={<Aboutus />} />
-					<Route exact path="investments" element={<AddInvestment />} />
-					<Route exact path="editInvestment/:investmentId" element={<AddInvestment />} />
-					<Route exact path="writeus" element={<Writeus />} />
-					<Route exact path="dashboard" element={<Dashboard />} />
-				</Routes>
+				<AppRouter />
 			</div>
 
 			<Footer />
--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -1,6 +1,19 @@
-import { Navigate, Route, Routes } from "react-router-dom";
 import useUser from "../../hooks/useUser";
-import { authRoutes, publicRoutes } from "./routes";
+import Articles from "../Articles/Articles"
+import ArticleFull from "../Articles/ArticleFull"
+import { Routes, Route } from "react-router-dom"
+import AddArticle from "../Articles/AddArticle"
+import AddCompany from "../Company/AddCompany"
+import Companies from "../Company/Companies"
+import CompanyFull from "../Company/CompanyFull"
+import Home from "../Home/Home"
+import AddBonus from "../Bonus/AddBonus"
+import Bonuses from "../Bonus/Bonuses"
+import BonusFull from "../Bonus/BonusFull"
+import Aboutus from "../Aboutus/Aboutus"
+import AddInvestment from "../Investment/AddInvestment"
+import Writeus from "../Writeus/Writeus"
+import Dashboard from "../Dashboard/Dashboard"
 
 // TODO
 export default function AppRouter() {
@@ -9,14 +22,30 @@ export default function AppRouter() {
 
 	return (
 		<Routes>
-			{user && authRoutes.map(({ path, Component }) =>
-				<Route key={path} exact path={path} element={Component} />
-			)}
-			{publicRoutes.map(({ path, Component }) =>
-				<Route key={path} exact path={path} element={Component} />
-			)}
+			<Route exact path="/" element={<Home />} />
+			<Route exact path="articles" element={<Articles />} />
+			<Route exact path="article/:articleId" element={<ArticleFull />} />
+			<Route exact path="companies" element={<Companies />} />
+			<Route exact path="company/:companyId" element={<CompanyFull />} />
+			<Route exact path="bonuses" element={<Bonuses />} />
+			<Route exact path="bonus/:bonusId" element={<BonusFull />} />
+			<Route exact path="aboutus" element={<Aboutus />} />
+			<Route exact path="writeus" element={<Writeus />} />
 
-			<Route path="*" element={<Navigate to="/login" replace />} />
+			{/* USER ROUTES */}
+			{user &&
+				<>
+					<Route exact path="add-article" element={<AddArticle />} />
+					<Route exact path="editArticle/:articleId" element={<AddArticle />} />
+					<Route exact path="add-company" element={<AddCompany />} />
+					<Route exact path="editCompany/:companyId" element={<AddCompany />} />
+					<Route exact path="add-bonus" element={<AddBonus />} />
+					<Route exact path="editBonus/:bonusId" element={<AddBonus />} />
+					<Route exact path="investments" element={<AddInvestment />} />
+					<Route exact path="editInvestment/:investmentId" element={<AddInvestment />} />
+					<Route exact path="dashboard" element={<Dashboard />} />
+				</>
+			}
 		</Routes>
 	)
 }
